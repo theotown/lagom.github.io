24b135d2225a18d29d74b66d76510301cecd4bbd
James Roper <james@jazzy.id.au>
2017-05-12 15:20:21 +1000

    Added 404 page (#69)

--- project/SimpleHTTPServer.scala
+++ project/SimpleHTTPServer.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.RouteResult.Complete
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
@@ -20,12 +21,19 @@ class SimpleHTTPServer(webDirectory: File, port: Int) extends Closeable {
   // needed for the future flatMap/onComplete in the end
   private implicit val executionContext = system.dispatcher
 
+  private val route404 = getFromFile(new File(webDirectory, "404.html"))
+    .andThen(_.map {
+      case Complete(response) => Complete(response.copy(status = 404))
+      case other => other
+    })
+
   private val route =
     getFromDirectory(webDirectory.getAbsolutePath) ~
       pathPrefix(Segments) { folderNameSeq =>
         val absoluteFolder = folderNameSeq.foldLeft(webDirectory)((acc, subfolder) => new File(acc, subfolder))
         getFromFile(new File(absoluteFolder, "index.html"))
-      }
+      } ~ route404
+
 
   val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(route, "localhost", port)
 

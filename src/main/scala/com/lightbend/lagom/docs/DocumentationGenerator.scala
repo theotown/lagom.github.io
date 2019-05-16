dc112f2893995ef11675e39939eec4d10b7e4a36
Renato Cavalcanti <renato@cavalcanti.be>
2019-05-16 19:08:24 +0200

    Lagom 1.4.13 is released (#188)

--- src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
+++ src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
@@ -29,7 +29,7 @@ object DocumentationGenerator extends App {
   // This impacts what gets displayed on the main documentation index.
   val stableVersions = Seq(
     VersionSummary("1.5.x", s"Lagom $currentLagomVersion (current stable release)"),
-    VersionSummary("1.4.x", s"Lagom 1.4.12")
+    VersionSummary("1.4.x", s"Lagom 1.4.13")
   )
 
   val previewVersions = Seq()

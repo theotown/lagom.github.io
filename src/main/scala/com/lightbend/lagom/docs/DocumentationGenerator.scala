7827e27b96801fff301a1e0af01c8e248f3e9c2c
Marcos Pereira <marcos.silva@gmail.com>
2019-07-12 10:14:09 -0400

    Lagom 1.6.0-M4 (#196)

--- src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
+++ src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
@@ -33,7 +33,7 @@ object DocumentationGenerator extends App {
   )
 
   val previewVersions = Seq(
-    VersionSummary("1.6.x", s"Lagom 1.6.0-M3 (preview)")
+    VersionSummary("1.6.x", s"Lagom 1.6.0-M4 (preview)")
   )
 
   val oldVersions = Seq(

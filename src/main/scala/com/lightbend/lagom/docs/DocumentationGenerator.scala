d20ae1988608fe87de03eef086b63c9b309eb9cd
Dale Wijnand <dale.wijnand@gmail.com>
2019-11-07 17:03:23 +0000

    Lagom 1.6.0-RC2 (preview)

--- src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
+++ src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
@@ -34,7 +34,7 @@ object DocumentationGenerator extends App {
   )
 
   val previewVersions = Seq(
-    VersionSummary("latest", s"Lagom 1.6.0-RC1 (preview)")
+    VersionSummary("latest", s"Lagom 1.6.0-RC2 (preview)")
   )
 
   val oldVersions = Seq(
